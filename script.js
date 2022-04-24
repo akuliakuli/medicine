64ea984d60728b09c192b1f52a9adde7e398c532
akuliakuli <kabby889@gmail.com>
2022-04-24 15:17:23 +0400

    file:edit

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const hamburger = document.querySelector(".hamburger"),
-      navMenu = document.querySelector(".nav-menu");
-
+      navMenu = document.querySelector(".nav-menu"),
+      parent = document.querySelector(".subcontainer");
 
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    parent.classList.toggle("show");
 })
